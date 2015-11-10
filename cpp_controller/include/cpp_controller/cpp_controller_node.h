d4c237a874cd1c9a94e3c12802a5d36e71ab651a
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-11-10 15:36:50 -0500

    [cpp_controller] Working with CppControllerNode class

--- cpp_controller/include/cpp_controller/cpp_controller_node.h
+++ cpp_controller/include/cpp_controller/cpp_controller_node.h
@@ -1,4 +1,19 @@
 #pragma once
-
 #include <ros/ros.h>
-// #include <cpp_controller/cpp_controller.h>
+#include "turtlesim/Pose.h"
+
+class CppControllerNode {
+
+public:
+    CppControllerNode();
+    virtual void init(ros::NodeHandle&);
+protected:
+    turtlesim::Pose target_position_;
+    // callbacks
+    void poseCallback(const turtlesim::Pose&);
+    geometry_msgs::Twist calculateControlInputs(turtlesim::Pose);
+    // subscribers
+    ros::Subscriber pose_sub_;
+    // publishers
+    ros::Publisher controller_pub_;
+};
--- cpp_controller/src/cpp_controller_node.cpp
+++ cpp_controller/src/cpp_controller_node.cpp
@@ -1,28 +1,51 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Pose.h"
 
-#include <sstream>
+#include <cpp_controller/cpp_controller_node.h>
 
+#include <math.h>
 
-// void CppControllerNodeInit(ros::NodeHandle &nh) {
-//
-//     // Subscribe to these topics
-//
-//     // Publish to these topics
-//
-// }
+CppControllerNode::CppControllerNode() {
 
-void poseCallback(const turtlesim::Pose pose_msg) {
-    ROS_INFO("(%f, %f)", pose_msg.x, pose_msg.y);
-    // return pose_msg
+    target_position_.x = 9.5;
+    target_position_.y = 5.0;
 }
 
-geometry_msgs::Twist calculateControlInputs(turtlesim::Pose pose_msg) {
+void CppControllerNode::init(ros::NodeHandle &nh) {
+
+    // Subscribe to these topics
+    pose_sub_ = nh.subscribe("turtle1/pose", 1, &CppControllerNode::poseCallback, this);
+
+    // Publish to these topics
+    controller_pub_ = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+}
+
+void CppControllerNode::poseCallback(const turtlesim::Pose &pose_msg) {
+
+    ROS_INFO("(x=%f, y=%f)", pose_msg.x, pose_msg.y);
+
+    geometry_msgs::Twist controls_msg = this->calculateControlInputs(pose_msg);
+    controller_pub_.publish(controls_msg);
+}
+
+geometry_msgs::Twist CppControllerNode::calculateControlInputs(turtlesim::Pose pose_msg) {
+
+    float epsilon = 2.0;
+    float vx = target_position_.x - pose_msg.x;
+    float vy = target_position_.y - pose_msg.y;
+    float theta = pose_msg.theta;
+
+    float u = vx*std::cos(theta) + vy*std::sin(theta);
+    float w = (1.0/epsilon)*(-vx*std::sin(theta) + vy*std::cos(theta));
+
+    //TODO: Add control input saturation constraints. It turns too fast.
+
     geometry_msgs::Twist controls_msg;
-    controls_msg.linear.x = 2.0;
-    controls_msg.angular.z = 2.0;
+    controls_msg.linear.x = u;
+    controls_msg.angular.z = w;
+
+    ROS_INFO("(u=%f, w=%f)", u, w);
     return controls_msg;
 }
 
@@ -32,31 +55,14 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "cpp_controller");
 
     ros::NodeHandle nh;
+    CppControllerNode cpp_controller_node = CppControllerNode();
+    cpp_controller_node.init(nh);
 
-    ros::Publisher controller_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
-    ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1000, poseCallback);
+    ros::Rate loop_rate(1);
 
-    ros::Rate loop_rate(10);
+    ROS_INFO("Done initializing! About to start spinning.");
+    ros::spin();
 
-    while (ros::ok())
-    {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-
-    turtlesim::Pose pose = turtlesim::Pose();
-
-    geometry_msgs::Twist controls_msg = calculateControlInputs(pose);
-
-    // ROS_INFO("%s", controls_msg); //TODO: How to do this in cpp?
-
-    controller_pub.publish(controls_msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-
-
-  return 0;
+    ROS_INFO("I'm spinning!");
+    return 0;
 }
