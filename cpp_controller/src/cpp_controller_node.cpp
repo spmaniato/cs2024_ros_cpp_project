1d8a8a86a5e9319b770edd97d09ce7589cb43d62
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-11-10 14:07:09 -0500

    [cpp_controller] Very simple node working

--- cpp_controller/src/cpp_controller_node.cpp
+++ cpp_controller/src/cpp_controller_node.cpp
@@ -1,14 +1,62 @@
-#include <cpp_controller/cpp_controller_node.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
 
-int main(int argc, char** argv)
+#include <sstream>
+
+
+// void CppControllerNodeInit(ros::NodeHandle &nh) {
+//
+//     // Subscribe to these topics
+//
+//     // Publish to these topics
+//
+// }
+
+void poseCallback(const turtlesim::Pose pose_msg) {
+    ROS_INFO("(%f, %f)", pose_msg.x, pose_msg.y);
+    // return pose_msg
+}
+
+geometry_msgs::Twist calculateControlInputs(turtlesim::Pose pose_msg) {
+    geometry_msgs::Twist controls_msg;
+    controls_msg.linear.x = 2.0;
+    controls_msg.angular.z = 2.0;
+    return controls_msg;
+}
+
+int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "cpp_controller");
 
-  // ros::NodeHandle nh;
-  // vigir_footstep_planning::FootstepPlannerNode footstep_planner_node;
-  // footstep_planner_node.init(nh);
+    ros::init(argc, argv, "cpp_controller");
 
-  ros::spin();
+    ros::NodeHandle nh;
+
+    ros::Publisher controller_pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+    ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1000, poseCallback);
+
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+
+    turtlesim::Pose pose = turtlesim::Pose();
+
+    geometry_msgs::Twist controls_msg = calculateControlInputs(pose);
+
+    // ROS_INFO("%s", controls_msg); //TODO: How to do this in cpp?
+
+    controller_pub.publish(controls_msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
 
   return 0;
 }
